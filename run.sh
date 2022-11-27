6dd3362188ad739d43965407bc9b642ad3c842b6
LemurDev <peyton.morse@outlook.com>
2022-11-27 11:25:45 -0500

    Hope this works

--- run.sh
+++ run.sh
@@ -8,33 +8,40 @@ apt install python3 build-essential g++ mono-complete elixir dotnet-sdk-6.0 gola
 # Python
 echo "Python"
 python3 python/hello.py
+printf "\n"
 
 # C
 echo "C"
 gcc c/hello.c -o c/hello_world.c
 c/hello_world.c
+printf "\n"
 
 # C#
 echo "C#"
 mcs -out:csharp/hello.exe csharp/hello.cs
 mono csharp/hello.exe
+printf "\n"
 
 # C++
 echo "C++"
 c++ cpp/hello.cpp -o cpp/hi.cpp
 cpp/hi.cpp
+printf "\n"
 
 # Elixir
 echo "Elixir"
 elixir elixir/hello.exs
+printf "\n"
 
 # F#
 echo "F#"
 dotnet fsi fsharp/hello.fsx
+printf "\n"
 
 # Golang
 echo "Golang"
 go run golang/hello.go
+printf "\n"
 
 # Java
 echo "Java"
@@ -42,49 +49,62 @@ cd java
 javac Main.java
 java Main
 cd ..
+printf "\n"
 
 # Javascript
 echo "Javascript"
 node js/hello.js
+printf "\n"
 
-# Kotlin -- FIX
-#echo "Kotlin"
-#kotlinc kotlin/hello.kt
-#java kotlin/HelloKt
+# Kotlin
+echo "Kotlin"
+cd kotlin
+kotlinc hello.kt
+java HelloKt
+cd ..
+printf "\n"
 
 # Lua
 echo "Lua"
 lua lua/hello.lua
+printf "\n"
 
 # Pascal
 echo "Pascal"
 pc pascal/hello.p > /dev/null 2>&1
 pascal/hello
+printf "\n"
 
 # Perl
 echo "Perl"
 perl perl/hello.pl
+printf "\n"
 
 # PHP
 echo "PHP"
 php php/hello.php
+printf "\n"
 
-# R -- Change output for this thing
+# R
 echo "R"
 Rscript r/hello.r
+printf "\n"
 
 # Bash
 echo "Bash"
 ./bash/hello.sh
+printf "\n"
 
 # Ruby
 echo "Ruby"
 ruby ruby/hello.rb
+printf "\n"
 
-# Rust -- FIX
-#echo "Rust"
-#rustc rust/hello.rs
-#rust/hello
+# Rust
+echo "Rust"
+rustc rust/hello.rs -o rust/hello
+rust/hello
+printf "\n"
 
 # Scala
 echo "Scala"
@@ -92,3 +112,4 @@ cd scala/
 scalac Hello.scala 
 scala Hello
 cd ..
+printf "\n"
